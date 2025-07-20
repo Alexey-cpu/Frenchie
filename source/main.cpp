a44b55824409eeb0d3c6d4f4cc728f578d58f160
Alexey Tykvinskiy <Tykvinsky.Alex@inbox.ru>
2025-07-20 15:09:11 +0500

    XML serializer prototype created

--- source/main.cpp
+++ source/main.cpp
@@ -14,6 +14,8 @@
 
 #include <FrenchieImGuiDemoLayer.hpp>
 
+#include <FrenchieCoreSerializableObject.hpp>
+
 using namespace Frenchie::Core;
 using namespace Frenchie::Renderer;
 using namespace Frenchie::Application;
@@ -45,246 +47,62 @@ public:
 //
 //---------------------------------------------------------------------------------
 
-#include "pugixml.hpp"
-
-struct Property
+void show_children(const Reference<Node>& _Node, std::string _Prefix = "")
 {
-    std::string Name;
-    std::any    Value;
+    std::cout << "\n";
 
-    template<typename Type> 
-    Type get() const
+    auto children = _Node->get_children();
+
+    for(auto&& child : children)
     {
-        try
+        std::cout << _Prefix << child->get_name() << "\n";
+
+        for(auto&& property : child->Properties)
         {
-            return std::any_cast<Type>(Value)
+            std::cout << _Prefix << property.get_name() << "\t" << property.to_string() << "\n";
         }
-        catch(...)
-        {
-            return Type();
-        } 
+
+        show_children(child, _Prefix + "\t");
     }
-
-    template<typename Type> 
-    void set(const Type& _Value) const
-    {
-        Value = _Value;
-    }
-
-    std::string to_string() const
-    {
-        try
-        {
-            return std::to_string(std::any_cast<bool>(Value));
-        }
-        catch(...)
-        {
-        }
-
-        try
-        {
-            return std::to_string(std::any_cast<float>(Value));
-        }
-        catch(...)
-        {
-        }
-
-        try
-        {
-            return std::to_string(std::any_cast<double>(Value));
-        }
-        catch(...)
-        {
-        }
-
-        try
-        {
-            return std::any_cast<std::string>(Value);
-        }
-        catch(...)
-        {
-        }
-
-        return std::string();
-    }
-
-    // nested types
-    struct TransparentComparator
-    {
-        using is_transparent = Property;
-
-    public:
-
-        bool operator()(const Property& _A, const Property& _B) const
-        {
-            return  _A.Name < _B.Name;
-        }
-    };
-};
-
-
-class Node
-{
-public:
-    Node(const std::string& _Name) : 
-        m_Name(_Name){}
-    
-    virtual ~Node(){}
-
-    std::string get_name() const
-    {
-        return m_Name;
-    }
-
-    std::vector<Node*> get_children() const
-    {
-        if(m_Children.empty()) 
-            return std::vector<Node*>();
-        
-        std::vector<Node*> children;
-
-        for(auto&& child : m_Children) 
-            children.push_back(child.get());
-
-        return children;
-    }
-
-    Node* append_child(const std::string& _Name)
-    {
-        m_Children.push_back(std::make_unique<Node>(_Name));
-        auto& child = m_Children.back();
-        child->m_Parent = this;
-        return child.get();
-    }
-
-    template<bool _Recursive = true>
-    void apply_to_children(const std::function<void(Node* _Object)>& _Callback) const
-    {
-        if(_Callback == nullptr) 
-            return;
-
-        for(auto&& child : m_Children) 
-        {
-            if(child == nullptr) 
-                continue;
-
-            _Callback(child.get());
-
-            if(_Recursive)
-                child->apply_to_children(_Callback);
-        }
-    }
-
-    std::set<Property, Property::TransparentComparator> Properties = 
-        std::set<Property, Property::TransparentComparator>();
-
-protected:
-
-    Node*       m_Parent = nullptr;
-    std::string m_Name   = std::string();
-
-    std::vector<std::unique_ptr<Node>> m_Children = 
-        std::vector<std::unique_ptr<Node>>();
-};
-
-template<typename T>
-class Format
-{
-public:
-    Format(){}
-    ~Format(){}
-
-    std::shared_ptr<Node> read(const std::filesystem::path& _Path)
-    {
-        return T::read(_Path);
-    }
-
-    bool write(std::shared_ptr<Node>& _Node, const std::filesystem::path& _Path)
-    {
-        return T::write(_Node);
-    }
-};
-
-class XML : public Format<XML>
-{
-public:
-    XML(){}
-    ~XML(){}
-
-    static std::shared_ptr<Node> read(const std::filesystem::path& _Path)
-    {
-        // try to parse project from XML file
-        pugi::xml_document doc;
-
-        auto status = doc.load_file(_Path.c_str()).status;
-
-        if(status != pugi::xml_parse_status::status_ok)
-        {
-            if(doc.load_file(&_Path.wstring()[0]).status != pugi::xml_parse_status::status_ok) 
-                return std::make_shared<Node>("EMPTY");
-        }
-
-        if(doc.empty()) 
-            return std::make_shared<Node>("EMPTY");
-        
-
-        struct Element
-        {
-            pugi::xml_node node   = pugi::xml_node();
-            Node*          object = nullptr;
-        };
-
-        std::shared_ptr<Node> root = std::make_shared<Node>(doc.first_child().name());
-        std::queue<Element>   queue = std::queue<Element>();
-        queue.push({doc.first_child(), root.get()});
-
-        std::cout << "doc.name() " << doc.first_child().name() << "\n";
-
-        while(!queue.empty())
-        {
-            auto top = queue.front();
-            queue.pop();
-
-            if(top.node.attribute("Type").as_string() == "Object")
-            {
-                auto child = top.object->append_child(top.node.name());
-
-                for(auto& element : top.node)
-                {
-                    if(top.node.attribute("Type").as_string() == "bool") 
-                        child->Properties.insert({element.name(), element.text().as_bool()});
-                    else if(top.node.attribute("Type").as_string() == "double") 
-                        child->Properties.insert({element.name(), element.text().as_double()});
-                    else if(top.node.attribute("Type").as_string() == "float") 
-                        child->Properties.insert({element.name(), element.text().as_float()});
-                    else if(top.node.attribute("Type").as_string() == "int") 
-                        child->Properties.insert({element.name(), element.text().as_int()});
-                    else if(top.node.attribute("Type").as_string() == "Object") 
-                        queue.push({element, child});
-                }
-            }
-        }
-
-        return root;
-    }
-};
+}
 
 int main(int, char**)
 {
-    auto node = XML().read("C:/SDK/Qt_Projects/OpenGL/logs/XML.xml");
+    std::shared_ptr<Node> document = 
+        std::make_shared<Node>("NewDocument");
 
-    std::cout << "root " << node->get_name() << "\n";
+    document->Properties.insert({"Bool", false});
+    document->Properties.insert({"Double", 12341232.123123});
+    document->Properties.insert({"Float", 1213.1231f});
+    document->Properties.insert({"Int", 101010});
 
-    node->apply_to_children([](Node* _Object)
+    auto child = document->append_child("Child");
+    child->Properties.insert({"Bool", false});
+    child->Properties.insert({"Double", 12341232.123123});
+    child->Properties.insert({"Float", 1213.1231f});
+    child->Properties.insert({"Int", 101010});
+
+    for(int i = 0 ; i < 1e1; i++)
     {
-        std::cout << _Object->get_name() << "\n";
-
-        for(auto&& property : _Object->Properties)
-        {
-            std::cout << property.Name << "\t" << property.to_string() << "\n";
-        }
+        child = child->append_child("Child");
+        child->Properties.insert({"Bool", false});
+        child->Properties.insert({"Double", 12341232.123123});
+        child->Properties.insert({"Float", 1213.1231f});
+        child->Properties.insert({"Int", 101010});
     }
-    );
+
+    // child = child->append_child("Child");
+    // child->Properties.insert({"Bool", false});
+    // child->Properties.insert({"Double", 12341232.123123});
+    // child->Properties.insert({"Float", 1213.1231f});
+    // child->Properties.insert({"Int", 101010});
+
+    XML().write(document, "C:/SDK/Qt_Projects/OpenGL/logs/NewDocument.xml");
+
+    std::cout << "document: \n";
+    auto node = XML().read("C:/SDK/Qt_Projects/OpenGL/logs/NewDocument.xml");
+
+    show_children(node);
 
     return 0;
 }
