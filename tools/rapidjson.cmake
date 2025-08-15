d0a0294c7ec3ffccaf6535b13e213a5d006e04bd
Alexey Tykvinskiy <Tykvinsky.Alex@inbox.ru>
2025-08-15 17:32:37 +0500

    rapidjson related things added

---
+++ tools/rapidjson.cmake
@@ -0,0 +1,11 @@
+# option(RAPIDJSON_BUILD_DOC "Build rapidjson documentation." ON)
+# option(RAPIDJSON_BUILD_EXAMPLES "Build rapidjson examples." ON)
+# option(RAPIDJSON_BUILD_TESTS "Build rapidjson perftests and unittests." ON)
+# option(RAPIDJSON_BUILD_THIRDPARTY_GTEST
+#     "Use gtest installation in `thirdparty/gtest` by default if available" OFF)
+
+# set(RAPIDJSON_BUILD_DOC OFF CACHE BOOL "Install SPDLOG" FORCE)
+# set(RAPIDJSON_BUILD_EXAMPLES OFF CACHE BOOL "Install SPDLOG" FORCE)
+# set(RAPIDJSON_BUILD_TESTS OFF CACHE BOOL "Install SPDLOG" FORCE)
+# set(RAPIDJSON_BUILD_THIRDPARTY_GTEST OFF CACHE BOOL "Install SPDLOG" FORCE)
+# add_subdirectory("tools/rapidjson/")
