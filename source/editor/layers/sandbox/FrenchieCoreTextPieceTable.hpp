2bc534bce5e217c801b838b999417066e88d5a7e
Alexey Tykvinskiy <a.tykvinkiy@zmeke.com>
2025-10-15 22:46:06 +0500

    one more piece table model

--- source/editor/layers/sandbox/FrenchieCoreTextPieceTable.hpp
+++ source/editor/layers/sandbox/FrenchieCoreTextPieceTable.hpp
@@ -11,32 +11,6 @@ namespace Frenchie
     {
         struct PieceTableRow
         {
-            struct PieceTableBuffer
-            {
-                PieceTableBuffer(const std::wstring& _Buffer) : Buffer(_Buffer)
-                {
-                    for (int i = 0; i < (int)Buffer.size(); i++)
-                    {
-                        if(Buffer[i] == '\n')
-                            Lines.push_back(i);
-                    }   
-                }
-
-                void push(const std::wstring& _What)
-                {
-                    Buffer.append(_What);
-
-                    for (int i = (int)(Buffer.size() - _What.size()); i < (int)Buffer.size(); i++)
-                    {
-                        if(Buffer[i] == '\n')
-                            Lines.push_back(i);
-                    }
-                }
-
-                std::wstring     Buffer;
-                std::vector<int> Lines;
-            };
-
             PieceTableRow(const std::wstring* _Buffer, int _Start, int _Length) : 
                 Buffer(_Buffer),
                 Start(_Start),
--- source/main.cpp
+++ source/main.cpp
@@ -11,83 +11,215 @@
 #include <FrenchieCoreTextPieceTable.hpp>
 
 #include <iostream>
+#include <string>
 
-void show(const Frenchie::Core::PieceTable& table)
+struct Buffer
 {
-    if(table.size() > 1024) 
+public:
+
+    Buffer(const std::wstring& _Text = std::wstring()) : m_Text(_Text)
     {
-        std::cout << table.size() << "\n";
-        return;
+        m_Lines.push_back(0);
+
+        for(int i = 0; i < (int)m_Text.size(); i++)        
+        {
+            if (m_Text[i] == '\n')
+                m_Lines.push_back(i);
+        }
     }
 
-    std::cout << Frenchie::Core::String::as_utf8(table.get_text()) << "\t" << table.size() << "\n\n";
-}
+    wchar_t at(const int& _Position) const
+    {
+        return m_Text[_Position];
+    }
+
+    int size() const
+    {
+        return m_Text.size();
+    }
+
+    int line_start_index(const int& _LineNumber) const
+    {        
+        return _LineNumber > 0 ? m_Lines[std::min<int>(_LineNumber - 1, m_Lines.size() - 1)] + 1 : 0;
+    }
+
+    int line_end_index(const int& _LineNumber) const
+    {
+        return m_Lines[std::min<int>(_LineNumber, m_Lines.size() - 1)];
+    }
+
+    int line_closest_to_buffer_offset(int _Offset) const
+    {
+        if(m_Lines.size() <= 0 || _Offset < 0)
+            return 0;
+
+        if(_Offset > m_Lines[m_Lines.size() - 1])
+            return m_Lines.size() - 1;
+
+        int low = 0;
+        int high = m_Lines.size() - 1;
+
+        while(low <= high) 
+        {
+            int mid = low + (high - low) / 2;
+
+            if (m_Lines[mid] == _Offset)
+                return mid;
+
+            if (m_Lines[mid] < _Offset)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return std::min<int>(low, m_Lines.size() - 1);
+    }
+
+    int lines_count() const
+    {
+        return (int)m_Lines.size();
+    }
+
+protected:
+    std::wstring     m_Text;
+    std::vector<int> m_Lines;
+};
+
+class Piece
+{
+public:
+
+    Piece(const Buffer* _Buffer, int _Start, int _Length) : 
+        m_Buffer(_Buffer),
+        m_Start(_Start),
+        m_Length(_Length){}
+    
+    const Buffer* buffer() const
+    {
+        return m_Buffer;
+    }
+
+    wchar_t at(int _Position) const
+    {
+        return m_Buffer->at(m_Start + _Position);
+    }
+
+    int start() const
+    {
+        return m_Start;
+    }
+
+    int end() const
+    {
+        return m_Start + m_Length;
+    }
+
+    int length() const
+    {
+        return m_Length;
+    }
+
+protected:
+
+    const int     m_Start      = 0;
+    const int     m_Length     = 0;
+    const Buffer* m_Buffer     = nullptr;
+};
+
+class PieceTable
+{
+public:
+
+    typedef std::list<Piece>::const_iterator const_iterator;
+    typedef std::list<Piece>::iterator iterator;
+
+    PieceTable(const std::wstring& _Text) : 
+        m_Immutable(_Text),
+        m_Appendable(std::wstring()),
+        m_Pieces({Piece(&m_Immutable, 0, (int)m_Immutable.size())}){}
+    
+    ~PieceTable(){}
+
+    void insert(const int& _Line, const int& _Position = 0, const std::wstring& _What = std::wstring())
+    {
+        // look for the line
+        int currentLine = 0;
+
+        for(auto it = begin(); it != end(); it++)
+        {
+            int firstLine = it->buffer()->line_closest_to_buffer_offset(it->start()) + currentLine;
+            int lastLine  = it->buffer()->line_closest_to_buffer_offset(it->end()) + currentLine;
+
+            std::cout << "buffer start " << it->start() << "\n";
+            std::cout << "buffer end   " << it->end() << "\n";
+
+            std::cout << "firstLine " << firstLine << "\n";
+            std::cout << "lastLine " << lastLine << "\n";
+
+            if(_Line >= firstLine && _Line <= lastLine)
+            {
+                std::cout << "found " << _Line << " offset " << it->buffer()->line_start_index(_Line) << "\n";
+
+                // int availableSpace    = it->buffer()->size() - it->buffer()->line_start_index(_Line);
+                // int insertionPosition = _Position;
+
+                // if(_Position > availableSpace)
+                // {
+
+                // }
+
+                break;
+            }
+
+            currentLine += (lastLine - firstLine) + 1;
+        }
+    }
+
+    void remove(const int& _Line, const int& _Position, const int& _Count)
+    {
+    }
+
+    const_iterator begin() const
+    {
+        return m_Pieces.begin();
+    }
+
+    const_iterator end() const
+    {
+        return m_Pieces.end();
+    }
+
+protected:
+    const   Buffer           m_Immutable;
+    mutable Buffer           m_Appendable;
+    mutable std::list<Piece> m_Pieces;
+};
 
 int main()
 {
-	std::wstring textBuffer = L"HelloWorld";
+    PieceTable table(L"Hello\nWorld");
 
-	// for (size_t j = 0; j < 20; j++)
-	// {
-	// 	for (size_t i = 0; i < 1; i++)
-	// 		textBuffer.append(L"for(int i = 0; i < 10; i++)");
+    table.insert(1, 7, L"123");
 
-	// 	textBuffer.append(L"\n");
-	// }
-    
-    Frenchie::Core::PieceTable table(textBuffer);
-    show(table);
-    
-    auto then = Frenchie::Core::Time::tic();
+    // std::wstring text = L"Hello\nWorld";
 
-    table.erase(2, 10);
-    show(table);
+    // Buffer buffer(text);
 
-    table.insert(0, L"Hello");
-    show(table);
+    // std::cout << "\n";
 
-    table.erase(2, 2);
-    show(table);
+    // int lineNumber = buffer.line_closest_to_buffer_offset(6);
 
-    std::cout << "after undo: \n";
-    table.undo();
-    show(table);
-
-    std::cout << "after undo: \n";
-    table.undo();
-    show(table);
-
-    std::cout << "after undo: \n";
-    table.undo();
-    show(table);
-
-    std::cout << "after redo: \n";
-    table.redo();
-    show(table);
-
-    std::cout << "after redo: \n";
-    table.redo();
-    show(table);
-
-    // int cursor = 0;
-
-    // for(int i = 0; i < 10; i++) 
+    // for(int i = buffer.line_start_index(lineNumber); i < buffer.line_end_index(lineNumber); i++)
     // {
-    //     table.insert(cursor++, std::to_wstring(i));
-    //     table.insert(cursor++, L" ");
-    //     show(table);
+    //     std::cout << Frenchie::Core::String::as_utf8(std::wstring(1, buffer.at(i))) << "\n";
     // }
 
-    // table.insert(5, L"456");
-    // show(table);
+    // int first = buffer.line_closest_to_buffer_offset(2);
+    // int last  = buffer.line_closest_to_buffer_offset(6);
 
-    // table.insert(0, L"789");
-    // show(table);
-
-    // table.insert(table.size(), L"101112");
-    // show(table);
-
-    std::cout << Frenchie::Core::Time::elapsed<std::chrono::milliseconds>(then, Frenchie::Core::Time::tic()) << "ms \n";
+    // std::cout << "first " << first << "\n";
+    // std::cout << "last  " << last << "\n";
+    // std::cout << "line count " << (last - first + 1) << "\t" << buffer.lines_count() << "\n";
 
     return 0;
 }
