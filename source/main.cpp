a821b323d1f47aa8bff00a48c40c05286c966760
Alexey Tykvinskiy <Tykvinsky.Alex@inbox.ru>
2025-11-03 11:37:08 +0500

    font atlas packer prototype created

--- source/main.cpp
+++ source/main.cpp
@@ -36,11 +36,29 @@
 #include <stdlib.h>
 #include <cstring>
 #include <string>
+#include <vector>
 
 #include <iostream>
 
 #include "stb_image_write.h" /* http://nothings.org/stb/stb_image_write.h */
 #include "stb_truetype.h" /* http://nothings.org/stb/stb_truetype.h */
+
+struct FontGeometry
+{
+    float PixelSizeScale = 1.f; // scale font to fit a given font size in pixels
+    int   fontAscent     = 0;   // distance from the glyph baseline to it's highest point 
+    int   fontDescent    = 0;   // distance fron the glyph baseline to it's lowest point
+    int   fontLineGap    = 0;   // recommended gap between text lines
+    int   UnicodeMin     = INT_MAX;
+    int   UnicodeMax     = INT_MIN;
+
+};
+
+struct Font
+{
+    stbtt_fontinfo* info;
+};
+
 
 void stb_free_font_info(stbtt_fontinfo* _Info)
 {
@@ -52,6 +70,27 @@ void stb_free_font_info(stbtt_fontinfo* _Info)
 
     if(_Info->userdata != nullptr)
         free(_Info->userdata);
+}
+
+unsigned char* stb_load_font_to_buffer(const char* _FilePathUTF8, int* _Size)
+{
+    // load font file
+    long size;
+    unsigned char* fontBuffer;
+    
+    FILE* fontFile = fopen(_FilePathUTF8, "rb");
+    fseek(fontFile, 0, SEEK_END);
+    size = ftell(fontFile);
+    fseek(fontFile, 0, SEEK_SET);
+    
+    fontBuffer = (unsigned char*)malloc(size);
+    
+    fread(fontBuffer, size, 1, fontFile);
+    fclose(fontFile);
+
+    *_Size = size;
+
+    return fontBuffer;
 }
 
 stbtt_fontinfo stb_open_ttf_file(const char* _FilePathUTF8)
@@ -116,7 +155,7 @@ int stb_get_text_line_width(stbtt_fontinfo* _FontInfo, const char* _Utf8Text, fl
     return x;
 }
 
-int main(int argc, const char * argv[])
+void render_colored_text_example()
 {
     // load .ttf
     auto fontInfo = stb_open_ttf_file(
@@ -151,12 +190,6 @@ int main(int argc, const char * argv[])
     int x = 0;
     int y = 0;
 
-    std::cout << "fontAscent  " << fontAscent << "\n";
-    std::cout << "fontDescent " << fontDescent << "\n";
-    std::cout << "fontLineGap " << fontLineGap << "\n";
-    std::cout << "textWidth  "  << textWidth << "\n";
-    std::cout << "textHeight "  << textHeight << "\n";
-
     int previous_codepoint = 0; // For kerning
 
     for (int i = 0; i < strlen(text.c_str()); ++i)
@@ -164,7 +197,7 @@ int main(int argc, const char * argv[])
         int codepoint = text.c_str()[i];
 
         // compute glyph horizontal metrics
-        int glyphAdvanceWidth     = 0; // The distance to advance the cursor after rendering the glyph.
+        int glyphAdvanceWidth    = 0; // The distance to advance the cursor after rendering the glyph.
 	    int glyphLeftSideBearing = 0; // The horizontal offset from the origin to the left edge of the glyph's bounding box
 
         int glyph_kern = // The adjustment of the horizontal space between individual glyph pairs to ensure they look balanced
@@ -216,7 +249,7 @@ int main(int argc, const char * argv[])
 
         previous_codepoint = codepoint;
     }
-    
+
     // colorify text
     int red   = 0;
     int green = 1;
@@ -242,6 +275,102 @@ int main(int argc, const char * argv[])
     
     free(bitmap);
     free(image);
+}
+
+// #define NUM_SIZES  16
+// #define NUM_GLYPHS glyphsCount
+
+int main(int argc, const char * argv[])
+{
+    // load .ttf font to a buffer
+    int ttf_buffer_size = 0;
+    unsigned char* ttf_buffer = stb_load_font_to_buffer("C:/SDK/Qt_Projects/OpenGL/shared/appData/fonts/Alice-Regular.ttf", &ttf_buffer_size);
+
+    // prepare font
+    stbtt_fontinfo fontInfo;
+    if (!stbtt_InitFont(&fontInfo, ttf_buffer, 0))
+    {
+        fontInfo.userdata = nullptr;
+        fontInfo.data     = nullptr;
+        free(ttf_buffer);
+        return -1;
+    }
+
+    // estimate appoximate atlas size
+    int MaxFontSize  = 72;
+    int MinFontSize  = 12;
+    int glyphsCount  = 0;
+    int fontsCount   = 0;
+    int firstUnicode = -1;
+
+    glyphsCount = 0;
+
+    // count font sizes
+    for (int fontSize = MaxFontSize, fontNumber = 0; fontSize >= MinFontSize; fontSize -= 4, fontsCount++);
+
+    // count available glyphs
+    for (unsigned int codepoint = 0; codepoint <= 0xFFFF; codepoint++)
+    {
+        if(!stbtt_FindGlyphIndex(&fontInfo, codepoint))
+            continue;
+
+        if(firstUnicode < 0)
+            firstUnicode = codepoint;
+
+        glyphsCount++;
+    }
+
+    // std::cout << "atlasWidth  " << atlasWidth << "\n";
+    // std::cout << "atlasHeight " << atlasHeight << "\n";
+    std::cout << "fontsCount  " << fontsCount << "\n";
+    std::cout << "glyphsCount " << glyphsCount << "\n";
+
+    // setup glyph info stuff, check stb_truetype.h for definition of structs
+    stbtt_pack_range* ranges = (stbtt_pack_range*)malloc(fontsCount*sizeof(stbtt_pack_range));
+
+    for (int fontSize = MaxFontSize, fontNumber = 0; fontSize >= MinFontSize; fontSize -= 4, fontNumber++)
+    {
+        stbtt_pack_range range;
+        range.font_size                        = (float)fontSize;
+        range.first_unicode_codepoint_in_range = firstUnicode;
+        range.array_of_unicode_codepoints      = NULL;
+        range.num_chars                        = glyphsCount;
+        range.chardata_for_range               = (stbtt_packedchar*)malloc(glyphsCount*sizeof(stbtt_packedchar));
+        ranges[fontNumber]                     = range;
+    }
+
+    // make a most likely large enough bitmap, adjust to font type, number of sizes and glyphs and oversampling
+    int textureSize  = 128;
+
+    unsigned char *bitmap = NULL;
+    
+    while (true)
+    {
+        bitmap = (unsigned char*)malloc(textureSize * textureSize);
+        stbtt_pack_context pc;
+        stbtt_PackBegin(&pc, bitmap, textureSize, textureSize, 0, 1, NULL);   
+        stbtt_PackSetOversampling(&pc, 1, 1); // say, choose 3x1 oversampling for subpixel positioning
+        std::cout << "texture size " << textureSize << "\n";
+
+        if(!stbtt_PackFontRanges(&pc, ttf_buffer, 0, ranges, fontsCount))
+        {
+			free(bitmap);
+			stbtt_PackEnd(&pc);
+			textureSize *= 2;
+		} 
+        else 
+        {
+			stbtt_PackEnd(&pc);
+			break;
+		}
+    }
+
+    stbi_write_png("atlas.png", textureSize, textureSize, 1, bitmap, textureSize);
+
+    // cleanup
+    free(ttf_buffer);
+    free(bitmap);
+    free(ranges);
 
     return 0;
 }
