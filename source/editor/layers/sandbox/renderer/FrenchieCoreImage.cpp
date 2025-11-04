bec811f480e4cfd6600690a7b6756bdb8db65e21
Alexey Tykvinskiy <Tykvinsky.Alex@inbox.ru>
2025-11-04 14:29:42 +0500

    filex and refactoring, font atlas packing algorithm enhanced

--- source/editor/layers/sandbox/renderer/FrenchieCoreImage.cpp
+++ source/editor/layers/sandbox/renderer/FrenchieCoreImage.cpp
@@ -7,76 +7,76 @@
 #include <FrenchieCoreFileSystem.hpp>
 
 using namespace Frenchie::Core;
-using namespace Frenchie::Core::Image;
 using namespace Frenchie::Core::String;
+using namespace Frenchie::Core::Renderer;
 using namespace Frenchie::Core::FileSystem;
 
 namespace Frenchie
 {
     namespace Core
     {
-        namespace Image
+        namespace Renderer
         {
-            const std::set<std::string> SUPPORTED_FILE_FORMATS =
+            const std::set<std::string> SUPPORTED_IMAGE_FORMATS =
                 std::set<std::string>({".png", ".jpg", ".tga", ".bmp"});
         }
     }
 }
 
-ImageInfo::ImageInfo(const std::filesystem::path& _Path)
+Image::Image(const std::filesystem::path& _Path)
 {
     // try to load image
-    if(SUPPORTED_FILE_FORMATS.find(utf8_to_lower(get_file_extention(_Path))) != SUPPORTED_FILE_FORMATS.end())
+    if(SUPPORTED_IMAGE_FORMATS.find(utf8_to_lower(get_file_extention(_Path))) != SUPPORTED_IMAGE_FORMATS.end() && 
+        std::filesystem::exists(_Path))
     {
-        m_Buffer = stbi_load(
-            convert_utf32_to_utf8(_Path.u32string()).c_str(),
-            &m_Width,
-            &m_Height,
-            &m_Channels,
-            0
-        );
+        m_Buffer = 
+            std::shared_ptr<unsigned char>(
+                stbi_load(
+                    convert_utf32_to_utf8(_Path.u32string()).c_str(), &m_Width, &m_Height, &m_Channels,0),
+                    [](unsigned char* _Buffer)
+                    {
+                        if(_Buffer != nullptr)
+                            stbi_image_free(_Buffer);
+                    }
+            );
     }
 }
 
-ImageInfo::ImageInfo(){}
+Image::Image(){}
 
-ImageInfo::~ImageInfo()
+Image::~Image(){}
+
+unsigned char* Image::get_buffer() const
 {
-    if(m_Buffer != nullptr)
-        stbi_image_free(m_Buffer);
+    return m_Buffer.get();
 }
 
-unsigned char* ImageInfo::get_buffer() const
-{
-    return m_Buffer;
-}
-
-int ImageInfo::get_width() const
+int Image::get_width() const
 {
     return m_Width;
 }
 
-int ImageInfo::get_height() const
+int Image::get_height() const
 {
     return m_Height;
 }
 
-int ImageInfo::get_channels() const
+int Image::get_channels() const
 {
     return m_Channels;
 }
 
-bool ImageInfo::is_valid() const
+bool Image::is_valid() const
 {
     return m_Buffer != nullptr && m_Width > 0 && m_Height > 0 && m_Channels > 0;
 }
 
-std::shared_ptr<ImageInfo> Frenchie::Core::Image::load_image(const std::filesystem::path& _Path)
+std::shared_ptr<Image> Frenchie::Core::Renderer::load_image(const std::filesystem::path& _Path)
 {
-    return Frenchie::Core::Memory::Cache<ImageInfo>::request(_Path.u32string(), _Path);
+    return Frenchie::Core::Memory::Cache<Image>::request(_Path.u32string(), _Path);
 }
 
-bool Frenchie::Core::Image::save_image_as_png(const std::shared_ptr<ImageInfo>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName)
+bool Frenchie::Core::Renderer::save_image_as_png(const std::shared_ptr<Image>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName)
 {
     if(!std::filesystem::exists(_Path.parent_path()))
         return false;
@@ -97,7 +97,7 @@ bool Frenchie::Core::Image::save_image_as_png(const std::shared_ptr<ImageInfo>& 
     );
 }
 
-bool Frenchie::Core::Image::save_image_as_jpg(const std::shared_ptr<ImageInfo>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName, const int& _Quality)
+bool Frenchie::Core::Renderer::save_image_as_jpg(const std::shared_ptr<Image>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName, const int& _Quality)
 {
     if(!std::filesystem::exists(_Path.parent_path()))
         return false;
@@ -118,7 +118,7 @@ bool Frenchie::Core::Image::save_image_as_jpg(const std::shared_ptr<ImageInfo>& 
     );
 }
 
-bool Frenchie::Core::Image::save_image_as_tga(const std::shared_ptr<ImageInfo>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName)
+bool Frenchie::Core::Renderer::save_image_as_tga(const std::shared_ptr<Image>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName)
 {
     if(!std::filesystem::exists(_Path.parent_path()))
         return false;
@@ -138,7 +138,7 @@ bool Frenchie::Core::Image::save_image_as_tga(const std::shared_ptr<ImageInfo>& 
     );
 }
 
-bool Frenchie::Core::Image::save_image_as_bmp(const std::shared_ptr<ImageInfo>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName)
+bool Frenchie::Core::Renderer::save_image_as_bmp(const std::shared_ptr<Image>& _Image, const std::filesystem::path& _Path, const std::u32string& _FileName)
 {
     if(!std::filesystem::exists(_Path.parent_path()))
         return false;
--- source/main.cpp
+++ source/main.cpp
@@ -43,22 +43,7 @@
 #include "stb_image_write.h" /* http://nothings.org/stb/stb_image_write.h */
 #include "stb_truetype.h" /* http://nothings.org/stb/stb_truetype.h */
 
-struct FontGeometry
-{
-    float PixelSizeScale = 1.f; // scale font to fit a given font size in pixels
-    int   fontAscent     = 0;   // distance from the glyph baseline to it's highest point 
-    int   fontDescent    = 0;   // distance fron the glyph baseline to it's lowest point
-    int   fontLineGap    = 0;   // recommended gap between text lines
-    int   UnicodeMin     = INT_MAX;
-    int   UnicodeMax     = INT_MIN;
-
-};
-
-struct Font
-{
-    stbtt_fontinfo* info;
-};
-
+#include <FrenchieCoreTime.hpp>
 
 void stb_free_font_info(stbtt_fontinfo* _Info)
 {
@@ -277,100 +262,348 @@ void render_colored_text_example()
     free(image);
 }
 
-// #define NUM_SIZES  16
-// #define NUM_GLYPHS glyphsCount
+// Core
+#include <FrenchieCoreStringUnicode.hpp>
+#include <FrenchieCoreMemoryCache.hpp>
+
+// STL
+#include <filesystem>
+
+namespace Fonts
+{
+    class Font;
+    class Atlas;
+
+    // RAII font
+    class Font
+    {
+    public:
+        Font(const std::filesystem::path& _Path, const int& _SizeInPixels = 12) : m_SizeInPixels(_SizeInPixels)
+        {
+            // load font file
+            FILE* fontFile = fopen(Frenchie::Core::String::convert_utf32_to_utf8(_Path.u32string()).c_str(), "rb");
+
+            if(!fontFile)
+                return;
+
+            // determine font file size
+            fseek(fontFile, 0, SEEK_END);
+            long fontFileSize = ftell(fontFile);
+            fseek(fontFile, 0, SEEK_SET);
+            
+            // read font file contents into a buffer
+            unsigned char* fontBuffer =
+                (unsigned char*)malloc(fontFileSize * sizeof(unsigned char));
+            
+            fread(fontBuffer, fontFileSize, 1, fontFile);
+            fclose(fontFile);
+
+            // prepare font
+            m_Info = std::shared_ptr<stbtt_fontinfo>(
+                (stbtt_fontinfo*)malloc(sizeof(stbtt_fontinfo)),
+                [this](stbtt_fontinfo* _Info)
+                {
+                    if(_Info == nullptr)
+                        return;
+                    
+                    free(_Info->data);
+                    free(_Info->userdata);
+                    free(_Info);
+                }
+            );
+
+            m_Info->data     = nullptr;
+            m_Info->userdata = nullptr;
+
+            if (!stbtt_InitFont(m_Info.get(), fontBuffer, 0))
+            {
+                free(fontBuffer);
+                return;
+            }
+
+            for(unsigned int codepoint = 0; codepoint <= 0xFFFF; codepoint++)
+            {
+                if(!stbtt_FindGlyphIndex(m_Info.get(), codepoint))
+                    continue;
+
+                m_UnicodeMin = std::min<int>(m_UnicodeMin, codepoint);
+                m_UnicodeMax = std::max<int>(m_UnicodeMax, codepoint);
+            }
+
+            m_IsDirty = true;
+        }
+
+        ~Font(){}
+
+        // getters
+        float get_max_font_size() const
+        {
+            return m_SizeMaximum;
+        }
+
+        float get_min_font_size() const
+        {
+            return m_SizeMinimum;
+        }
+
+        unsigned char* get_font_buffer() const
+        {
+            return m_Info != nullptr ? m_Info->data : nullptr;
+        }
+
+        int get_size_in_pixels() const
+        {
+            return m_SizeInPixels;
+        }
+
+        float get_pixel_size_scale() const
+        {
+            if(m_IsDirty) on_dirty();
+            return m_SizeInPixelScale;
+        }
+
+        int get_ascent() const
+        {
+            if(m_IsDirty) on_dirty();
+            return m_Ascent;
+        }
+
+        int get_descent() const
+        {
+            if(m_IsDirty) on_dirty();
+            return m_Descent;
+        }
+
+        int get_linegap() const
+        {
+            if(m_IsDirty) on_dirty();
+            return m_LineGap;
+        }
+
+        int get_unciode_min() const
+        {
+            return m_UnicodeMin;
+        }
+
+        int get_unciode_max() const
+        {
+            return m_UnicodeMax;
+        }
+
+        int is_dirty() const
+        {
+            return m_IsDirty;
+        }
+
+        std::shared_ptr<Atlas> get_atlas() const
+        {
+            return m_Atlas;
+        }
+
+        const std::shared_ptr<stbtt_fontinfo> get_info() const
+        {
+            return m_Info;
+        }
+
+        // setters
+        void set_size_in_pixels(const int& _Size)
+        {
+            m_SizeInPixels = _Size;
+            m_IsDirty      = true;
+        }
+
+    protected:
+
+        void on_dirty() const
+        {
+            // compute font pixel size scale
+            m_SizeInPixelScale = stbtt_ScaleForPixelHeight(m_Info.get(), (float)m_SizeInPixels);
+
+            // compute font vertical metrics
+            stbtt_GetFontVMetrics(m_Info.get(), &m_Ascent, &m_Descent, &m_LineGap);
+
+            m_Ascent  = (int)roundf((float)m_Ascent  * m_SizeInPixelScale);
+            m_Descent = (int)roundf((float)m_Descent * m_SizeInPixelScale);
+            m_LineGap = (int)roundf((float)m_LineGap * m_SizeInPixelScale);
+            m_IsDirty = false;
+        }
+
+        // info
+        mutable float                           m_SizeMaximum      = 512.f;   // maximum font size
+        mutable float                           m_SizeMinimum      = 2.f;     // minimum font size
+        mutable int                             m_SizeInPixels     = 12;      // font size in pixels
+        mutable float                           m_SizeInPixelScale = 1.f;     // scale font to fit a given font size in pixels
+        mutable int                             m_Ascent           = 0;       // distance from the glyph baseline to it's highest point 
+        mutable int                             m_Descent          = 0;       // distance fron the glyph baseline to it's lowest point
+        mutable int                             m_LineGap          = 0;       // recommended gap between text lines
+        mutable int                             m_UnicodeMin       = INT_MAX; // starting Unicode symbol
+        mutable int                             m_UnicodeMax       = INT_MIN; // ending Unicode symbol
+        mutable bool                            m_IsDirty          = true;    // dirty flag
+        mutable std::shared_ptr<Atlas>          m_Atlas            = nullptr; // STB true type font atlas
+        mutable std::shared_ptr<stbtt_fontinfo> m_Info             = nullptr; // STB true type font info
+    };
+
+    class Atlas final
+    {
+    public:
+
+        Atlas(const Font* _Font)
+        {
+            if(_Font == nullptr)
+                return;
+
+            m_Chars = std::shared_ptr<stbtt_packedchar>(
+                (stbtt_packedchar*)malloc(sizeof(stbtt_packedchar) * (_Font->get_unciode_max() - _Font->get_unciode_min())),
+                [](stbtt_packedchar* _Range)
+                {
+                    if(_Range != nullptr)
+                        free(_Range);
+                }
+            );
+
+            // make a most likely large enough bitmap, adjust to font type, number of sizes and glyphs and oversampling
+            int atlasWidth     = 0;
+            int atlasHeight    = 0;
+            int atlasMaxHeight = 16;
+            int atlasMaxWidth  = 32;
+            
+            while (true)
+            {
+                atlasWidth  = 0;
+                atlasHeight = 0;
+                
+                for (int codepoint = _Font->get_unciode_min(); codepoint < _Font->get_unciode_max();)
+                {
+                    int width = 0;
+                    int maxHeight = INT_MIN;
+
+                    while (true)
+                    {
+                        // compute glyph bounding box
+                        int glyphXmin = 0;
+                        int glyphYmin = 0;
+                        int glyphXmax = 0;
+                        int glyphYmax = 0;
+
+                        stbtt_GetCodepointBitmapBox(
+                            _Font->get_info().get(),
+                            codepoint,
+                            _Font->get_pixel_size_scale(),
+                            _Font->get_pixel_size_scale(),
+                            &glyphXmin,
+                            &glyphYmin,
+                            &glyphXmax,
+                            &glyphYmax
+                        );
+
+                        int glyphWidth  = (glyphXmax - glyphXmin);
+                        int glyphHeight = (glyphYmax - glyphYmin);
+
+                        width    += std::max<int>(1, glyphWidth);
+                        maxHeight = std::max<int>(glyphHeight, maxHeight);
+
+                        ++codepoint;
+
+                        if(width >= atlasMaxWidth)
+                        {
+                            width -= std::max<int>(1, glyphWidth);
+                            width = atlasMaxWidth;
+                            break;
+                        }
+                        else
+                        {
+                        }
+                    }
+
+                    atlasWidth  = std::max<int>(atlasWidth, width);
+                    atlasHeight += std::max<int>(maxHeight, 1);
+                }
+
+                atlasMaxWidth *= 2;
+                atlasMaxWidth  = std::min<int>(atlasMaxWidth, 2048);
+
+                if(atlasHeight <= atlasMaxHeight)
+                    break;
+
+                atlasMaxHeight *= 2;
+                atlasMaxHeight  = std::min<int>(atlasMaxHeight, 2048);
+            }
+
+            // pack atlas
+            m_Bitmap        = nullptr;
+            m_TextureWidth  = atlasWidth;
+            m_TextureHeight = atlasHeight;
+
+            while (true)
+            {
+                m_Bitmap = std::shared_ptr<unsigned char>(
+                    (unsigned char*)malloc(m_TextureWidth * m_TextureHeight),
+                    [](unsigned char* _Bitmap)
+                    {
+                        if(_Bitmap != nullptr)
+                        {
+                            free(_Bitmap);
+                            _Bitmap = nullptr;
+                        }
+                    }
+                );
+
+                stbtt_pack_context pc;
+                stbtt_PackBegin(&pc, m_Bitmap.get(), m_TextureWidth, m_TextureHeight, 0, 1, NULL);   
+                stbtt_PackSetOversampling(&pc, 1, 1);
+
+                if(!stbtt_PackFontRange(
+                    &pc,
+                    _Font->get_font_buffer(),
+                    0,
+                    (float)_Font->get_size_in_pixels(),
+                    _Font->get_unciode_min(),
+                    _Font->get_unciode_max() - _Font->get_unciode_min(),
+                    m_Chars.get()))
+                {
+                    stbtt_PackEnd(&pc);
+
+                    if(m_TextureWidth < 4096)
+                        m_TextureWidth *= 2;
+                    else 
+                        m_TextureHeight *= 2;
+                } 
+                else 
+                {
+                    stbtt_PackEnd(&pc);
+                    break;
+                }
+            }
+        }
+        
+        ~Atlas(){}
+
+        std::shared_ptr<unsigned char>    m_Bitmap        = nullptr;
+        std::shared_ptr<stbtt_packedchar> m_Chars         = nullptr;
+        int                               m_TextureWidth  = 32;
+        int                               m_TextureHeight = 32;
+    };
+}
 
 int main(int argc, const char * argv[])
 {
-    // load .ttf font to a buffer
-    int ttf_buffer_size = 0;
-    unsigned char* ttf_buffer = stb_load_font_to_buffer("C:/SDK/Qt_Projects/OpenGL/shared/appData/fonts/Alice-Regular.ttf", &ttf_buffer_size);
+    std::filesystem::path path("C:/SDK/Qt_Projects/OpenGL/shared/appData/fonts/Alice-Regular.ttf");
 
-    // prepare font
-    stbtt_fontinfo fontInfo;
-    if (!stbtt_InitFont(&fontInfo, ttf_buffer, 0))
-    {
-        fontInfo.userdata = nullptr;
-        fontInfo.data     = nullptr;
-        free(ttf_buffer);
-        return -1;
-    }
+    auto font  = Frenchie::Core::Memory::Cache<Fonts::Font>::request(path.u32string(), path, 128);
 
-    // estimate appoximate atlas size
-    int MaxFontSize  = 72;
-    int MinFontSize  = 12;
-    int glyphsCount  = 0;
-    int fontsCount   = 0;
-    int firstUnicode = -1;
+    auto then = Frenchie::Core::Time::tic();
 
-    glyphsCount = 0;
+    auto atlas = Frenchie::Core::Memory::Cache<Fonts::Atlas>::request(
+        U"Alice-Regular-Atlas", font.get());
 
-    // count font sizes
-    for (int fontSize = MaxFontSize, fontNumber = 0; fontSize >= MinFontSize; fontSize -= 4, fontsCount++);
+    std::cout << Frenchie::Core::Time::elapsed<std::chrono::milliseconds>(then, Frenchie::Core::Time::tic()) << "ms \n";
 
-    // count available glyphs
-    for (unsigned int codepoint = 0; codepoint <= 0xFFFF; codepoint++)
-    {
-        if(!stbtt_FindGlyphIndex(&fontInfo, codepoint))
-            continue;
-
-        if(firstUnicode < 0)
-            firstUnicode = codepoint;
-
-        glyphsCount++;
-    }
-
-    // std::cout << "atlasWidth  " << atlasWidth << "\n";
-    // std::cout << "atlasHeight " << atlasHeight << "\n";
-    std::cout << "fontsCount  " << fontsCount << "\n";
-    std::cout << "glyphsCount " << glyphsCount << "\n";
-
-    // setup glyph info stuff, check stb_truetype.h for definition of structs
-    stbtt_pack_range* ranges = (stbtt_pack_range*)malloc(fontsCount*sizeof(stbtt_pack_range));
-
-    for (int fontSize = MaxFontSize, fontNumber = 0; fontSize >= MinFontSize; fontSize -= 4, fontNumber++)
-    {
-        stbtt_pack_range range;
-        range.font_size                        = (float)fontSize;
-        range.first_unicode_codepoint_in_range = firstUnicode;
-        range.array_of_unicode_codepoints      = NULL;
-        range.num_chars                        = glyphsCount;
-        range.chardata_for_range               = (stbtt_packedchar*)malloc(glyphsCount*sizeof(stbtt_packedchar));
-        ranges[fontNumber]                     = range;
-    }
-
-    // make a most likely large enough bitmap, adjust to font type, number of sizes and glyphs and oversampling
-    int textureSize  = 128;
-
-    unsigned char *bitmap = NULL;
-    
-    while (true)
-    {
-        bitmap = (unsigned char*)malloc(textureSize * textureSize);
-        stbtt_pack_context pc;
-        stbtt_PackBegin(&pc, bitmap, textureSize, textureSize, 0, 1, NULL);   
-        stbtt_PackSetOversampling(&pc, 1, 1); // say, choose 3x1 oversampling for subpixel positioning
-        std::cout << "texture size " << textureSize << "\n";
-
-        if(!stbtt_PackFontRanges(&pc, ttf_buffer, 0, ranges, fontsCount))
-        {
-			free(bitmap);
-			stbtt_PackEnd(&pc);
-			textureSize *= 2;
-		} 
-        else 
-        {
-			stbtt_PackEnd(&pc);
-			break;
-		}
-    }
-
-    stbi_write_png("atlas.png", textureSize, textureSize, 1, bitmap, textureSize);
-
-    // cleanup
-    free(ttf_buffer);
-    free(bitmap);
-    free(ranges);
+    stbi_write_png("atlas.png",
+        atlas->m_TextureWidth,
+        atlas->m_TextureHeight,
+        1,
+        atlas->m_Bitmap.get(),
+        atlas->m_TextureWidth);
 
     return 0;
 }
