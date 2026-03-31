d65c4f957d75afea5d989739a8af89082d4eb264
Alexey Tykvinskiy <Tykvinsky.Alex@inbox.ru>
2026-03-31 14:46:59 +0500

    cmake external packages configurators fixed

--- tools/sdl3.cmake
+++ tools/sdl3.cmake
@@ -7,9 +7,9 @@ FetchContent_Declare(
     GIT_TAG origin/release-3.4.x
     OVERRIDE_FIND_PACKAGE)
 
-FetchContent_MakeAvailable(SDL3)
-
 # configure SDL
 set(SDL_SHARED OFF CACHE BOOL "" FORCE)
 set(SDL_STATIC ON CACHE BOOL "" FORCE)
 set(SDL_DISABLE_INSTALL ON CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(SDL3)
