45f59a53b88df65920dd9b3793e697c67c4441a1
Alexey Tykvinskiy <a.tykvinkiy@zmeke.com>
2026-05-15 12:00:08 +0500

    textures support completed

--- frenchie/source/backends/rendering/Metal/FrenchieApplicationRenderingBackendMetal.mm
+++ frenchie/source/backends/rendering/Metal/FrenchieApplicationRenderingBackendMetal.mm
@@ -32,9 +32,14 @@ namespace Frenchie
             // state
             id<MTLRenderPipelineState>  rendererPipeLineState = nil;
             id<MTLDepthStencilState>    rendererDepthState    = nil;
-            id<MTLSamplerState>         rendererSamplerState  = nil;
 
             CAMetalLayer*               layer        = nil;
+        };
+
+        struct ApplicationRenderingBackendMetalTextureData
+        {
+            id<MTLTexture>      Texture      = nil;
+            id<MTLSamplerState> SamplerState = nil;
         };
 
         struct ApplicationRenderingBackendMetalShaderUniforms
@@ -204,18 +209,8 @@ fragment float4 fragment_main(
         depthDescriptor.depthCompareFunction               = MTLCompareFunctionLess;
         depthDescriptor.depthWriteEnabled                  = YES;
 
-        // textures sampler descriptor
-        MTLSamplerDescriptor* samplerDescriptor = [MTLSamplerDescriptor new];
-        samplerDescriptor.minFilter             = MTLSamplerMinMagFilterLinear;
-        samplerDescriptor.magFilter             = MTLSamplerMinMagFilterLinear;
-        samplerDescriptor.mipFilter             = MTLSamplerMipFilterLinear;
-        samplerDescriptor.sAddressMode          = MTLSamplerAddressModeRepeat; // Wrap mode
-        samplerDescriptor.tAddressMode          = MTLSamplerAddressModeRepeat; // Wrap mode
-        samplerDescriptor.normalizedCoordinates = YES; // Use 0.0 - 1.0 range
-
         Metal->rendererPipeLineState = [Metal->gpu newRenderPipelineStateWithDescriptor:pipelineDescriptor error:&error];
         Metal->rendererDepthState    = [Metal->gpu newDepthStencilStateWithDescriptor:depthDescriptor];
-        Metal->rendererSamplerState  = [Metal->gpu newSamplerStateWithDescriptor:samplerDescriptor];
 
         if(error != nil)
         {
@@ -229,7 +224,6 @@ fragment float4 fragment_main(
         [vertexDescriptor release];
         [pipelineDescriptor release];
         [depthDescriptor release];
-        [Metal->rendererSamplerState release];
     }
 
     return true;
@@ -324,21 +318,128 @@ ApplicationRenderingBackendTexture ApplicationRenderingBackend::construct_textur
     textureDescriptor.height      = _Height;
     textureDescriptor.pixelFormat = MTLPixelFormatRGBA8Unorm;//MTLPixelFormatBGRA8Unorm;
 
-    // create texture using descriptor
+    // create texture
     id<MTLTexture> texture = [Metal->gpu newTextureWithDescriptor:textureDescriptor];
 
-    // copy input data into the texture
     MTLRegion region = {{ 0, 0, 0 }, {NSUInteger(_Width), NSUInteger(_Height), 1}};
 
-    NSUInteger bytesPerRow = 4 * NSUInteger(_Width);
+    NSUInteger bytesPerRow = 0;
+
+    switch(_Format)
+    {
+        case ApplicationRenderingBackendTextureFormat_::ApplicationRenderingBackendTextureFormat_ALPHA:
+        bytesPerRow = 1 * NSUInteger(_Width);
+            break;
+
+        case ApplicationRenderingBackendTextureFormat_::ApplicationRenderingBackendTextureFormat_RGB:
+        bytesPerRow = 3 * NSUInteger(_Width);
+            break;
+
+        case ApplicationRenderingBackendTextureFormat_::ApplicationRenderingBackendTextureFormat_RGBA:
+        bytesPerRow = 4 * NSUInteger(_Width);
+            break;
+    }
 
     [texture replaceRegion:region mipmapLevel:0 withBytes:_RawBuffer bytesPerRow:bytesPerRow];
 
-    // clean descriptor
+    // create texture sampler
+    MTLSamplerDescriptor* samplerDescriptor = [MTLSamplerDescriptor new];
+    samplerDescriptor.normalizedCoordinates = YES; // Use 0.0 - 1.0 range
+
+    // set the texture wrapping parameters
+    switch (_Wrap)
+    {
+    case ApplicationRenderingBackendTextureWrapMode_::ApplicationRenderingBackendTextureWrapMode_Repeat:
+        samplerDescriptor.sAddressMode = MTLSamplerAddressModeRepeat;
+        samplerDescriptor.tAddressMode = MTLSamplerAddressModeRepeat;
+        break;
+    
+    case ApplicationRenderingBackendTextureWrapMode_::ApplicationRenderingBackendTextureWrapMode_Mirrored:
+        samplerDescriptor.sAddressMode = MTLSamplerAddressModeMirrorRepeat;
+        samplerDescriptor.tAddressMode = MTLSamplerAddressModeMirrorRepeat;
+        break;
+
+    case ApplicationRenderingBackendTextureWrapMode_::ApplicationRenderingBackendTextureWrapMode_ClampToEdge:
+        samplerDescriptor.sAddressMode = MTLSamplerAddressModeClampToEdge;
+        samplerDescriptor.tAddressMode = MTLSamplerAddressModeClampToEdge;
+        break;
+
+    case ApplicationRenderingBackendTextureWrapMode_::ApplicationRenderingBackendTextureWrapMode_ClampToBorder:
+        samplerDescriptor.sAddressMode = MTLSamplerAddressModeClampToBorderColor;
+        samplerDescriptor.tAddressMode = MTLSamplerAddressModeClampToBorderColor;
+        break;
+    default:
+        samplerDescriptor.sAddressMode = MTLSamplerAddressModeRepeat;
+        samplerDescriptor.tAddressMode = MTLSamplerAddressModeRepeat;
+        break;
+    }
+    
+    // set minifying filter
+    switch (_MinFilter)
+    {
+    case ApplicationRenderingBackendTextureMinFilter_::ApplicationRenderingBackendTextureMinFilter_Linear:
+        samplerDescriptor.minFilter = MTLSamplerMinMagFilterLinear;
+        break;
+    
+    case ApplicationRenderingBackendTextureMinFilter_::ApplicationRenderingBackendTextureMinFilter_Nearest:
+        samplerDescriptor.minFilter = MTLSamplerMinMagFilterNearest;
+        break;
+
+    case ApplicationRenderingBackendTextureMinFilter_::ApplicationRenderingBackendTextureMinFilter_LinearMipMapLinear:
+        samplerDescriptor.minFilter = MTLSamplerMinMagFilterLinear;
+        samplerDescriptor.mipFilter = MTLSamplerMipFilterLinear;
+        break;
+
+    case ApplicationRenderingBackendTextureMinFilter_::ApplicationRenderingBackendTextureMinFilter_LinearMipMapNearest:
+        samplerDescriptor.minFilter = MTLSamplerMinMagFilterLinear;
+        samplerDescriptor.mipFilter = MTLSamplerMipFilterNearest;
+        break;
+
+    case ApplicationRenderingBackendTextureMinFilter_::ApplicationRenderingBackendTextureMinFilter_NearestMipMapLinear:
+        samplerDescriptor.minFilter = MTLSamplerMinMagFilterNearest;
+        samplerDescriptor.mipFilter = MTLSamplerMipFilterLinear;
+        break;
+
+    case ApplicationRenderingBackendTextureMinFilter_::ApplicationRenderingBackendTextureMinFilter_NearestMipMapNearest:
+        samplerDescriptor.minFilter = MTLSamplerMinMagFilterNearest;
+        samplerDescriptor.mipFilter = MTLSamplerMipFilterNearest;
+        break;
+
+    default:
+        samplerDescriptor.minFilter = MTLSamplerMinMagFilterLinear;
+        samplerDescriptor.mipFilter = MTLSamplerMipFilterLinear;
+        break;
+    }
+
+    // set magnifying filter
+    switch (_MaxFilter)
+    {
+    case ApplicationRenderingBackendTextureMaxFilter_::ApplicationRenderingBackendTextureMaxFilter_Linear:
+        samplerDescriptor.magFilter = MTLSamplerMinMagFilterLinear;
+        break;
+    
+    case ApplicationRenderingBackendTextureMaxFilter_::ApplicationRenderingBackendTextureMaxFilter_Nearest:
+        samplerDescriptor.magFilter = MTLSamplerMinMagFilterNearest;
+        break;
+
+    default:
+        samplerDescriptor.magFilter = MTLSamplerMinMagFilterLinear;
+        break;
+    }
+
+    id<MTLSamplerState> samplerState  = [Metal->gpu newSamplerStateWithDescriptor:samplerDescriptor];
+
+    // clean-up
     [textureDescriptor release];
 
+    ApplicationRenderingBackendMetalTextureData* textureData =
+        new ApplicationRenderingBackendMetalTextureData();
+
+    textureData->Texture      = texture;
+    textureData->SamplerState = samplerState;
+
     return ApplicationRenderingBackendTexture(
-        reinterpret_cast<uintptr_t>(texture),
+        reinterpret_cast<uintptr_t>(textureData),
         _Width,
         _Height,
         gs_color_rgba(255, 255, 255, 255),
@@ -350,10 +451,21 @@ ApplicationRenderingBackendTexture ApplicationRenderingBackend::construct_textur
 
 void ApplicationRenderingBackend::destroy_texture(const ApplicationRenderingBackendTexture& _Texture)
 {
-    id<MTLTexture> texture = !_Texture.is_null() ? reinterpret_cast<id<MTLTexture>>(_Texture.Ptr) : nil;
+    ApplicationRenderingBackendMetalTextureData* textureData =
+        !_Texture.is_null() ?
+            reinterpret_cast<ApplicationRenderingBackendMetalTextureData*>(_Texture.Ptr) :
+                nil;
 
-    if(texture != nil)
-        [texture release];
+    if(textureData == nullptr)
+        return;
+
+    if(textureData->Texture != nil)
+        [textureData->Texture release];
+    
+    if(textureData->SamplerState != nil)
+        [textureData->SamplerState release];
+    
+    delete textureData;
 }
 
 bool ApplicationRenderingBackend::begin_render(
@@ -416,7 +528,7 @@ bool ApplicationRenderingBackend::begin_render(
     MTLRenderPassDescriptor* pass = [MTLRenderPassDescriptor renderPassDescriptor];
 
     // clear color
-    pass.colorAttachments[0].clearColor  = MTLClearColorMake(0., 0.2, 0., 1);
+    pass.colorAttachments[0].clearColor  = MTLClearColorMake(0.0, 0.2, 0.0, 1.0);
     pass.colorAttachments[0].loadAction  = MTLLoadActionClear;
     pass.colorAttachments[0].storeAction = MTLStoreActionStore;
     pass.colorAttachments[0].texture     = Metal->surface.texture;
@@ -450,30 +562,42 @@ void ApplicationRenderingBackend::render_mesh(
         return;
 
     // setup shader uniforms
-    ApplicationRenderingBackendMetalShaderUniforms uniforms;
-
-    for(int i = 0; i < 4; i++)
     {
-        for(int j = 0; j < 4; j++)
-            uniforms.Projection.columns[i][j] = _MeshProjectionMatrix[i][j];
+        ApplicationRenderingBackendMetalShaderUniforms uniforms;
+
+        for(int i = 0; i < _MeshProjectionMatrix.columns(); i++)
+        {
+            for(int j = 0; j < _MeshProjectionMatrix.rows(); j++)
+                uniforms.Projection.columns[i][j] = _MeshProjectionMatrix[i][j];
+        }
+
+        [Metal->encoder setVertexBytes:
+            &uniforms
+            length:sizeof(ApplicationRenderingBackendMetalShaderUniforms)
+            atIndex:1];
     }
 
-    [Metal->encoder setVertexBytes:
-        &uniforms
-        length:sizeof(ApplicationRenderingBackendMetalShaderUniforms)
-        atIndex:1];
+    // bind texture
+    {
+        ApplicationRenderingBackendMetalTextureData* textureData =
+            !_Texture.is_null() ?
+                reinterpret_cast<ApplicationRenderingBackendMetalTextureData*>(_Texture.Ptr) : 
+                    nullptr;
 
-    // setup texture
-    id<MTLTexture> texture = !_Texture.is_null() ? reinterpret_cast<id<MTLTexture>>(_Texture.Ptr) : nil;
-
-    [Metal->encoder setFragmentTexture:texture atIndex:0];
-    [Metal->encoder setFragmentSamplerState:Metal->rendererSamplerState atIndex:0];
+        if(textureData != nullptr)
+        {
+            [Metal->encoder setFragmentTexture:textureData->Texture atIndex:0];
+            [Metal->encoder setFragmentSamplerState:textureData->SamplerState atIndex:0];
+        }
+    }
 
     // render primitives
     [Metal->encoder drawIndexedPrimitives:
         MTLPrimitiveTypeTriangle
         indexCount:_MeshIndexesCount - _MeshIndexesOffset
-        indexType:sizeof(Frenchie::Application::ApplicationRenderingBackendMeshVertexIndex) == 2 ? MTLIndexTypeUInt16 : MTLIndexTypeUInt32
+        indexType:sizeof(Frenchie::Application::ApplicationRenderingBackendMeshVertexIndex) == 2 ?
+            MTLIndexTypeUInt16 :
+                MTLIndexTypeUInt32
         indexBuffer:Metal->indexBuffer
         indexBufferOffset:_MeshIndexesOffset * sizeof(ApplicationRenderingBackendMeshVertexIndex)];
 }
