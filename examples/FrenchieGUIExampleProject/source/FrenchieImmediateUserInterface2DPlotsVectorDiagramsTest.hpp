2ff189290c71a0fe9fd002829ce9d55bba217e98
Alexey Tykvinskiy <Tykvinsky.Alex@inbox.ru>
2026-06-03 14:46:03 +0500

    slight code refactor

--- examples/FrenchieGUIExampleProject/source/FrenchieImmediateUserInterface2DPlotsVectorDiagramsTest.hpp
+++ examples/FrenchieGUIExampleProject/source/FrenchieImmediateUserInterface2DPlotsVectorDiagramsTest.hpp
@@ -18,14 +18,13 @@ namespace Frenchie
 
         protected:
 
-            std::shared_ptr<ImmediateUserInterfaceContextLayer> m_UI            {nullptr};
+            std::shared_ptr<ImmediateUserInterfaceContextLayer> m_UI {nullptr};
 
             std::vector<gs_vec4f>    m_Vectror;
             std::vector<gs_color>    m_Colors;
             std::vector<std::string> m_Names;
-
-            int                                                 m_PlotWidgetSettings  {ImmediateUserInterfaceNodeSettings_::ImmediateUserInterfaceNodeSettings_None};
-            bool                                                m_PlotWidgetDrawLegend{true};
+            int                      m_PlotWidgetSettings  {ImmediateUserInterfaceNodeSettings_::ImmediateUserInterfaceNodeSettings_None};
+            bool                     m_PlotWidgetDrawLegend{true};
         };
     }
 }
