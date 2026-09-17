d5750ec2e5b15bcda9a7bc87105e4c9a2c99da7c
Alexey Tykvinskiy <Tykvinsky.Alex@inbox.ru>
2026-09-17 18:38:01 +0500

    drag & drop test window slightly modified

--- examples/FrenchieGUIExampleProject/source/FrenchieImmediateUserIntefaceDragAndDropTest.cpp
+++ examples/FrenchieGUIExampleProject/source/FrenchieImmediateUserIntefaceDragAndDropTest.cpp
@@ -146,7 +146,8 @@ void FrenchieImmediateUserIntefaceDragAndDropTest::frame_update()
                     m_UI->current_bounding_box().Min,
                     m_UI->current_bounding_box().Max,
                     std::any_cast<gs_color>(m_CanvasColor),
-                    m_UI->m_Renderer->calculate_transform_matrix((float)m_UI->current_depth()));
+                    m_UI->m_Renderer->calculate_transform_matrix((float)m_UI->current_depth()),
+                    m_UI->m_Style.get_frames_radius());
             
                 std::any dropData = m_UI->drop();
 
